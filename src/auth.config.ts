c18ef1667339ebc31c77307a745735462d46ce6a
Erik Hansen <erikhansenmujica@gmail.com>
2023-11-15 16:15:20 +0000

    middleware fix

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -6,16 +6,16 @@ export const authConfig = {
     signIn: "/login",
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl, url } }) {
+    authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.endsWith("dashboard");
       const isOnLogin = nextUrl.pathname.endsWith("login");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return Response.redirect("https://app.skyderdigital.com/login");
+        return Response.redirect(new URL("/login", nextUrl));
       } else if (isOnLogin) {
         if (isLoggedIn)
-          return Response.redirect("https://app.skyderdigital.com/dashboard");
+          return Response.redirect(new URL("/dashboard", nextUrl));
       }
       return true;
     },
